775a2b5e45810033808db944fd8c886cae84fd4c
Vladislav Borisov <vladborisov.vbnyc@gmail.com>
2018-12-08 15:08:45 -0500

    Initial Model Complete
    Commented to load existing 'digitizer.h5'

--- src/main.py
+++ src/main.py
@@ -4,6 +4,8 @@ print("\n>importing tensorflow, input data sets")
 import tensorflow as tf
 import keras
 import keras.backend as kbe
+from keras.models import load_model
+from keras import metrics
 import numpy
 import matplotlib.pyplot as plot
 from matplotlib import rcParams
@@ -11,6 +13,11 @@ import math
 from mlxtend.data import loadlocal_mnist
 import random
 
+
+label_names = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+
+
+'''
 # METRIC REDEFINITION
 def get_categorical_accuracy_keras(y_true, y_pred):
     return kbe.mean(kbe.equal(kbe.argmax(y_true, axis=1), kbe.argmax(y_pred, axis=1)))
@@ -30,9 +37,9 @@ def plotter(i, predictions_arr, correct_label, img):
     else:
         color = 'red'
 
-    plot.xlabel("{} {:2.0f}% ({})".format(predicted_label,
+    plot.xlabel("{} {:2.0f}% ({})".format(label_names[predicted_label],
                                           100 * numpy.max(predictions_arr),
-                                          correct_label),
+                                          label_names[correct_label]),
                 color=color)
 
 def plot_value_array(i, predictions_arr, correct_label):
@@ -41,7 +48,7 @@ def plot_value_array(i, predictions_arr, correct_label):
     plot.xticks([])
     plot.yticks([])
 
-    thisplot = plot.bar(range(10), predictions_arr, color="#777777")
+    thisplot = plot.bar(range(62), predictions_arr, color="#777777")
     plot.ylim([0, 1])
     predicted_label = numpy.argmax(predictions_arr)
 
@@ -50,15 +57,23 @@ def plot_value_array(i, predictions_arr, correct_label):
 
 
 
+# training_images, training_labels = loadlocal_mnist(
+#     images_path='./data/train-images-idx3-ubyte',
+#     labels_path='./data/train-labels-idx1-ubyte')
+# testing_images, testing_labels = loadlocal_mnist(
+#     images_path='./data/t10k-images-idx3-ubyte',
+#     labels_path='./data/t10k-labels-idx1-ubyte')
+
 training_images, training_labels = loadlocal_mnist(
-    images_path='./data/train-images-idx3-ubyte',
-    labels_path='./data/train-labels-idx1-ubyte')
+    images_path='./data/emnist-byclass-train-images-idx3-ubyte',
+    labels_path='./data/emnist-byclass-train-labels-idx1-ubyte')
 testing_images, testing_labels = loadlocal_mnist(
-    images_path='./data/t10k-images-idx3-ubyte',
-    labels_path='./data/t10k-labels-idx1-ubyte')
+    images_path='./data/emnist-byclass-test-images-idx3-ubyte',
+    labels_path='./data/emnist-byclass-test-labels-idx1-ubyte')
 
 numpy.set_printoptions(linewidth=1800)
 gridSize = int(math.sqrt(training_images.shape[1]))
+
 
 print('\n------------------------------\n')
 print('ORIGINALS:')
@@ -89,51 +104,6 @@ print('Images: ', testing_images.shape[0], '  x  ', testing_images[1].shape)
 print('Labels: ', len(testing_labels))
 print('Label distribution: ', numpy.bincount(testing_labels))
 
-'''
-#Scaling Values from 0-255 to 0-1.0
-training_images = training_images / 255.0
-testing_images = testing_images / 255.0
-
-
-item_num = numpy.arange(100)
-for x in range(0,100):
-    item_num[x] = random.randint(0,60000)
-
-#Plot Preferences
-#plot.tick_params(axis='both', which='major', labelsize=6)
-rcParams.update({'figure.autolayout': True})
-plot.figure(figsize=(50,300))
-
-#PRINT RANDOM 100 ITEMS
-for i in range(100):
-    plot.subplot(20, 5, i+1)
-    plot.grid(False)
-    #plot.yticks(numpy.arange(0, 28, 1.0))
-    #plot.xticks(numpy.arange(0, 28, 1.0))
-    plot.xticks([])
-    plot.yticks([])
-    plot.imshow(training_images[item_num[i]], cmap = plt.cm.binary)
-    plot.xlabel(training_labels[item_num[i]], fontsize=200)
-plot.savefig("pleasework.png")
-'''
-
-
-#PRINT INDIVIDUAL RANDOM ITEM
-'''
-myrand = random.randint(0,60000)
-
-
-plot.figure(figsize=(10,10))
-plot.grid(True)
-plot.tick_params(axis='both', which='major', labelsize=6)
-plot.yticks(numpy.arange(0, 28, 1.0))
-plot.xticks(numpy.arange(0, 28, 1.0))
-plot.xlabel(training_labels[myrand])
-plot.imshow(training_images[myrand], cmap = plt.cm.binary)
-plot.colorbar()
-plot.savefig("example.png")
-'''
-
 training_images = keras.utils.normalize(training_images, axis=1)
 testing_images = keras.utils.normalize(testing_images, axis=1)
 
@@ -141,25 +111,31 @@ model = keras.models.Sequential()
 model.add(keras.layers.Flatten(input_shape=(28,28)))                                    # Brings our 28x28 array back into a flat 1d aray
 model.add(keras.layers.Dense(128, activation=tf.nn.relu, bias_initializer='zeros'))     # Rectified Linear Unit - max(x,0)
 model.add(keras.layers.Dense(128, activation=tf.nn.relu, bias_initializer='zeros'))     # Rectified Linear Unit - max(x,0)
-model.add(keras.layers.Dense(10, activation=tf.nn.softmax, bias_initializer='zeros' ))  # Multiclass Probabalistic Output
+model.add(keras.layers.Dense(62, activation=tf.nn.softmax, bias_initializer='zeros' ))  # Multiclass Probabalistic Output
 
 model.compile(optimizer=keras.optimizers.Adam(lr=1e-5),                                 # RMSprop w/ momentum
               loss='sparse_categorical_crossentropy',
-              metrics=[get_categorical_accuracy_keras]
+              metrics=[keras.metrics.mae]
               )
 
 model.summary()
 
 model.fit(training_images, training_labels, epochs=1)                                   # Arbitrary Epoch Cutoff
+model.save('digitizer.h5')
 
+del model
+'''
+model = load_model('digitizer.h5')
+model.summary()
+
+
+'''
 val_loss, val_acc = model.evaluate(testing_images, testing_labels)
 print(val_loss, val_acc)
 
-#model.save('digitizer.model')
 predictions = model.predict([testing_images])
-
-num_rows = 10
-num_cols = 5
+num_rows = 50
+num_cols = 1
 num_images = num_rows*num_cols
 plot.figure(figsize=(2*2*num_cols, 2*num_rows))
 for i in range(num_images):
@@ -168,6 +144,24 @@ for i in range(num_images):
   plot.subplot(num_rows, 2*num_cols, 2*i+2)
   plot_value_array(i, predictions, testing_labels)
   plot.savefig('testMaster.png')
+
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 '''
@@ -180,6 +174,8 @@ for i in range(100):
                 testing_images[i][j][k] = 4.4
     print(testing_images[i])
 '''
+
+
 
 
 
