b49a6bdfbb2fb995c2ac389efa6699108c28ced4
Boris Avrumov <bavrumov@hotmail.com>
2018-12-09 16:28:34 -0500

    responsive canvas

---
+++ src/SavableCanvas.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import {LiterallyCanvasReactComponent, tools, ClearButton} from 'literallycanvas';
+
+export class SavableCanvas extends Component {
+  static propTypes = {
+    initialize : PropTypes.boolean,
+  }
+  
+  render() {
+    //var lc = LiterallyCanvasReactComponent.init({getElementById("test")}, {imageSize: {width: 300, height: 300}})
+    console.log(this.props.initialize);
+    var size = {width: "420", height: "420"};
+    return (
+      <div id="test">
+        <LiterallyCanvasReactComponent
+          
+          imageURLPrefix = "./static/canvas/"
+          keyboardShortcuts = "false"
+          backgroundColor = "white"
+          imageSize = {size}
+          onInit = {(lc) => {console.log("initialized with", lc);}}
+          strokeWidths = {[30]}
+          tools = {[tools.Pencil]}
+          
+        >
+        <ClearButton></ClearButton>
+        </LiterallyCanvasReactComponent>
+        
+      </div>
+    )
+  }
+}
+
+export default SavableCanvas
