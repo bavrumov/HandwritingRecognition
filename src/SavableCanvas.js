4c680eeca53f689823a5cb5a7657dd3203e6981f
Boris Avrumov <bavrumov@hotmail.com>
2018-12-15 01:48:35 -0500

    Canvas now looks marvelous

--- src/SavableCanvas.js
+++ src/SavableCanvas.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import {LiterallyCanvasReactComponent, tools, ClearButton} from 'literallycanvas';
 import {Button} from 'reactstrap'
+// import { BrowserDownloads } from '@tensorflow/tfjs-core/dist/io/browser_files';
 
 var clr =<ClearButton></ClearButton>;
+var LC;
 
 export class SavableCanvas extends Component {
   static propTypes = {
@@ -11,37 +13,45 @@ export class SavableCanvas extends Component {
   }
   
   clear = () => {
-    clr.click();
+    //let clr = document.getElementsByClassName("lc-clear");
+    console.log("This is our clear button:")
     console.log(clr);
   }
+
+  submit = () => {
+    console.log("TensorFlow is going to take the following canvas as a parameter:");
+    console.log(document.getElementById("test").firstChild.firstChild.childNodes[1]);
+    //console.log(document.getElementsByClassName("lc-drawing")[0]);
+  }
   
+  componentDidMount() {
+    document.getElementsByClassName("with-gui")[0].style.left=0;
+  }
 
   render() {
-    //var lc = LiterallyCanvasReactComponent.init({getElementById("test")}, {imageSize: {width: 300, height: 300}})
     console.log(this.props.initialize);
     var size = {width: "420", height: "420"};
-    var LC = 
+    LC = 
     <LiterallyCanvasReactComponent
-          
-    imageURLPrefix = "./static/canvas/"
-    keyboardShortcuts = "false"
-    backgroundColor = "white"
-    imageSize = {size}
-    onInit = {(lc) => {console.log("initialized with", lc);}}
-    strokeWidths = {[65]}
-    tools = {[tools.Pencil]}
-    toolbarPosition = "bottom"
-    defaultStrokeWidth = {65}
-  >
-  {clr}
-  </LiterallyCanvasReactComponent>;
+      imageURLPrefix = "./static/canvas/"
+      keyboardShortcuts = "false"
+      backgroundColor = "white"
+      imageSize = {size}
+      onInit = {(lc) => {console.log("initialized with", lc);}}
+      strokeWidths = {[65]}
+      tools = {[tools.Pencil]}
+      toolbarPosition = "hidden"
+      defaultStrokeWidth = {65}
+    >
+    {clr}
+    </LiterallyCanvasReactComponent>;
 
     return (
       <div id="test">
         {LC}
         <div>
           <Button onClick={this.clear} className="canvasButtons" id="clearButton" color="danger">Clear Letter</Button>
-          <Button className="canvasButtons" id="submitButton" color="success">Submit Letter</Button>
+          <Button onClick={this.submit} className="canvasButtons" id="submitButton" color="success">Submit Letter</Button>
         </div>
       </div>
     )
